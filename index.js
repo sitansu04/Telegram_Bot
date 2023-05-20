4d3bbf2e3095afb42c7f380f0bf8d4e3706ee510
Sitansu Mandal <sitansugcelt@gmail.com>
2023-05-20 11:38:31 +0530

    telegram bot makes wikipedia article post to an particular group

---
+++ index.js
@@ -0,0 +1,43 @@
+const TelegramBot = require("node-telegram-bot-api");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+const telegram_token = '6198725543:AAG8L9OU_KxSKgC5oua4KTQlH8ktQobXsPE';
+const bot = new TelegramBot(telegram_token, { polling: true });
+
+const url = "https://en.wikipedia.org/api/rest_v1/page/random/summary";
+
+let title, summery;
+
+async function fetched() {
+  let data = await fetch(url);
+  let out = await data.json();
+
+  title = out.title;
+  summery = out.extract;
+  console.log(out, `\n`, out.title, `\n`, out.extract);
+}
+fetched();
+
+bot.on("message", (msg) => {
+  try {
+    var text="wiki"
+    const chatId = msg.chat.id;
+
+    if (msg.text.toString().toLowerCase().indexOf(text) === 0) {
+      fetched();
+      // send a message to the chat acknowledging receipt of their message
+      bot.sendMessage(chatId, `Title : ${title}\n Summery : \n ${summery}`);
+    }
+
+    if(msg.text.toString().toLowerCase()=="hi" || msg.text.toString().toLowerCase()=="hello"){
+        bot.sendMessage(chatId, "Hello  " + msg.from.first_name);
+    }
+    if(msg.text.toString().toLowerCase()=="bye"){
+        bot.sendMessage(chatId, "Bye! Have fun  " + msg.from.first_name);
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
+});
