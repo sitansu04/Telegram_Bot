2efcdb1a7077cda3513e5158dc1a30587367e557
Sitansu Mandal <sitansugcelt@gmail.com>
2023-05-20 12:21:52 +0530

    token added in env

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const telegram_token = '6198725543:AAG8L9OU_KxSKgC5oua4KTQlH8ktQobXsPE';
+const telegram_token = process.env.bot_token;
 const bot = new TelegramBot(telegram_token, { polling: true });
 
 const url = "https://en.wikipedia.org/api/rest_v1/page/random/summary";
@@ -21,7 +22,7 @@ fetched();
 
 bot.on("message", (msg) => {
   try {
-    var text="wiki"
+    var text = "wiki";
     const chatId = msg.chat.id;
 
     if (msg.text.toString().toLowerCase().indexOf(text) === 0) {
@@ -30,14 +31,16 @@ bot.on("message", (msg) => {
       bot.sendMessage(chatId, `Title : ${title}\n Summery : \n ${summery}`);
     }
 
-    if(msg.text.toString().toLowerCase()=="hi" || msg.text.toString().toLowerCase()=="hello"){
-        bot.sendMessage(chatId, "Hello  " + msg.from.first_name);
+    if (
+      msg.text.toString().toLowerCase() == "hi" ||
+      msg.text.toString().toLowerCase() == "hello"
+    ) {
+      bot.sendMessage(chatId, "Hello  " + msg.from.first_name);
     }
-    if(msg.text.toString().toLowerCase()=="bye"){
-        bot.sendMessage(chatId, "Bye! Have fun  " + msg.from.first_name);
+    if (msg.text.toString().toLowerCase() == "bye") {
+      bot.sendMessage(chatId, "Bye! Have fun  " + msg.from.first_name);
     }
-
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 });
